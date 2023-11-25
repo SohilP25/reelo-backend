482f0b3c91b01c61d90d54c1e944c2106f9f411b
Sohil Pathan <srpathan254@gmail.com>
2023-11-25 18:40:11 +0530

    port fixed

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import express from "express";
 import connectDB from "./config/db_connection.js";
 import router from "./routes/index.js";
 import bodyParser from "body-parser";
-import 'dotenv/config'
+import 'dotenv/config';
+const PORT = process.env.PORT || 3030;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("Server is running on port 3000");
 });
